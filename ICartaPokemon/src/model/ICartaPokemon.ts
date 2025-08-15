853f62739619c06b1b7bf9597734c6ad58a29641
Sofia Josafá <sofiajosafa19@gmail.com>
2025-08-15 13:03:03 -0300

    Delete ICartaPokemon directory

--- ICartaPokemon/src/model/ICartaPokemon.ts
+++
@@ -1,9 +0,0 @@
-export interface ICartaPokemon {
-    idCarta: String
-    nomePokemon: String
-    tipo: String
-    hp: number  
-    ataque: number
-    idCarta1: String
-    idCarta2: String
-  }
